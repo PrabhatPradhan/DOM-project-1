a0296ea5d8701db08c6ba3fd64772eddc86bf7a9
Prabhat <mmrprabhujii@gmail.com>
2024-02-28 21:13:09 +0530

    adding

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
-console.log("Prabhat");
+let div1 = document.getElementById("div1");
+div1.style.backgroundColor = "yellow";
+div1.style.height= "220px";
+div1.style.width= "320px";
+div1.style.margin=" 20px 30px"
+
+
+let para = document.getElementById('para');
+para.style.color = "blue";
+para.style.padding = "10px 10px";
+para.style.fontSize= "18px";
+para.style.fontWeight="bold";
